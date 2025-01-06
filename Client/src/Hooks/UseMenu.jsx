98133f847d4959aae836da1fb198ac49fba71e91
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-01-06 19:21:15 +0600

    added server

--- Client/src/Hooks/UseMenu.jsx
+++ Client/src/Hooks/UseMenu.jsx
@@ -4,7 +4,7 @@ const UseMenu = () => {
     const [menu, setMenu] = useState([]);
     const [loader, setLoader] = useState(true);
       useEffect(() => {
-        fetch("menu.json")
+        fetch("http://localhost:5000/menu")
           .then((res) => res.json())
           .then((data) => {
             setLoader(false);
